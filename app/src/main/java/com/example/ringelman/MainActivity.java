5536f64a0a90bc307027fa2fba498439de7ea9a5
胡百川 <3161700849@qq.com>
2023-10-30 12:49:21 +0800

    计算黑度并确定等级

--- app/src/main/java/com/example/ringelman/MainActivity.java
+++ app/src/main/java/com/example/ringelman/MainActivity.java
@@ -1,15 +1,95 @@
 package com.example.ringelman;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_cam;
+    Button btn_cal;
+    Uri image_uri;
+    ImageView iv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(getResources().getColor(R.color.green));
         setContentView(R.layout.activity_main);
+
+        iv1=findViewById(R.id.iv1);
+        btn_cam=findViewById(R.id.btn_cam);
+        btn_cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File image_file=new File(getExternalCacheDir(),"temp.jpg");
+                if(image_file.exists()){
+                    image_file.delete();
+                }
+                try {
+                    image_file.createNewFile();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                image_uri= FileProvider.getUriForFile(getApplicationContext(),
+                        "com.example.ringelman.image_uri",
+                        image_file);
+                Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+                startActivityForResult(intent,1);
+            }
+        });
+
+        btn_cal=findViewById(R.id.btn_cal);
+        btn_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, ResultActivity.class);
+                if(iv1.getDrawable()!=null) {
+                    Bitmap bitmap = ((BitmapDrawable) iv1.getDrawable()).getBitmap();
+                    Bundle bundle=new Bundle();
+                    bundle.putBinder("bitmap",new BitmapBinder(bitmap));
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this, "请选择图片", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        switch (requestCode){
+            case 1:
+                if(resultCode==RESULT_OK){
+                    Bitmap bitmap=null;
+                    try {
+                        bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(image_uri));
+                        iv1.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+            default:break;
+        }
     }
 }
--- app/src/main/java/com/example/ringelman/ResultActivity.java
+++ app/src/main/java/com/example/ringelman/ResultActivity.java
@@ -1,17 +1,85 @@
 package com.example.ringelman;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
+
 public class ResultActivity extends AppCompatActivity {
+
+    ImageView iv2;
+    TextView tv_dat,tv_lvl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(getResources().getColor(R.color.green));
         setContentView(R.layout.result);
+        Bundle bundle=getIntent().getExtras();
+        BitmapBinder bitmapBinder=(BitmapBinder)bundle.getBinder("bitmap");
+        Bitmap bitmap=bitmapBinder.getBitmap();
+        iv2=findViewById(R.id.iv2);
+        iv2.setImageBitmap(bitmap);
+        tv_dat=findViewById(R.id.tv_dat);
+        tv_lvl=findViewById(R.id.tv_lvl);
+        double data=ringelmanEmittance(bitmap);
+        BigDecimal d=new BigDecimal(data*100);
+        double data1=d.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+        tv_dat.setText(String.valueOf(data1)+"%");
+        double level=5*data;
+        int level1= (int) Math.round(level);
+        tv_lvl.setText(String.valueOf(level1));
+    }
+    public static Bitmap linGraham(Bitmap srcBitmap){
+        int width=srcBitmap.getWidth();
+        int height=srcBitmap.getHeight();
+        Bitmap dstBitmap=Bitmap.createBitmap(width,height,
+                Bitmap.Config.RGB_565);
+        int[] pixels=new int[width*height];
+        srcBitmap.getPixels(pixels,0,width,0,0,width,height);
+        double threshold=getGrayAvg(pixels,pixels.length);
+        for(int i=0;i<pixels.length;i++){
+            double gray=getGrayLevel(pixels[i]);
+            if(gray>threshold) {
+                pixels[i] = Color.WHITE;
+            }else{
+                    pixels[i]=Color.BLACK;
+                }
+        }
+        dstBitmap.setPixels(pixels,0,width,0,0,width,height);
+        return dstBitmap;
+    }
+    public static double ringelmanEmittance(Bitmap srcBitmap){
+        int width=srcBitmap.getWidth();
+        int height=srcBitmap.getHeight();
+        Bitmap dstBitmap=Bitmap.createBitmap(width,height,
+                Bitmap.Config.RGB_565);
+        int[] pixels=new int[width*height];
+        srcBitmap.getPixels(pixels,0,width,0,0,width,height);
+        double threshold=getGrayAvg(pixels,pixels.length);
+        double percent=1-threshold/255;
+        return percent;
+    }
+    public static double getGrayAvg(int[] pixels,int length){
+        double sum=0;
+        for(int i=0;i<length;i++){
+            double gray=getGrayLevel(pixels[i]);
+            sum+=gray;
+        }
+        double grayAvg=sum/length;
+        return grayAvg;
+    }
+    public static double getGrayLevel(int pixel){
+        int red= Color.red(pixel);
+        int green=Color.green(pixel);
+        int blue=Color.blue(pixel);
+        return (0.299*red+0.587*green+0.114*blue);
     }
 }
