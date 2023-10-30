80d63e3b905719c0f00274ff5fcc99f5dd46ad1b
胡百川 <3161700849@qq.com>
2023-10-30 10:44:03 +0800

    界面切换

---
+++ app/src/main/java/com/example/ringelman/BitmapBinder.java
@@ -0,0 +1,15 @@
+package com.example.ringelman;
+
+import android.graphics.Bitmap;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class BitmapBinder extends Binder {
+    private Bitmap bitmap;
+    BitmapBinder(Bitmap bitmap){
+        this.bitmap=bitmap;
+    }
+    Bitmap getBitmap(){
+        return bitmap;
+    }
+}
